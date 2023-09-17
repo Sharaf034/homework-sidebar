2b72bbe129706511efa4880fa9094a5911d65a24
Sharaf034 <sharafet@code.edu.az>
2023-09-17 16:52:25 +0400

    Add file

---
+++ assets/js/script.js
@@ -0,0 +1,174 @@
+"use strict"
+
+// ---------------------------------------------------------- repeat classwork
+
+
+// let addBtn = document.querySelector(".text button");
+// let inputText = document.querySelector(".text input");
+// let ul = document.querySelector(".text-area");
+// let span =document.querySelector(".text error-message")
+
+// addBtn.addEventListener("click", function () {
+//     if (inputText.value.trim() == "") {
+//         // alert("Can't be empty");----------------
+//         span.classList.remove("d-none")
+//         return;
+//     }
+
+//     span.classList.add("d-none")
+
+//     let li = document.createElement("li");
+//     li.className = "list-group-item";
+//     li.innerText = inputText.value;
+
+//     ul.append(li);
+//     inputText.value = "";
+
+//     li.onclick = function () {
+//         this.remove();
+//     }
+// })
+
+
+// console.log(document.querySelector("h1").parentNode.parentNode.parentNode);
+// console.log(document.querySelector("h1").nextElementSibling.nextElementSibling);
+// console.log(document.querySelector("h1").parentNode.previousElementSibling.firstElementChild.innerText)
+
+// console.log(document.querySelector("h1").closest(".container"))
+
+
+
+// let sidebar = document.querySelector(".sidebar");
+
+// let openIcon = document.querySelector(".sidebar .open-icon");
+
+// let closeIcon = document.querySelector(".sidebar .close-icon");
+
+
+// openIcon.addEventListener("click", function(){
+//     sidebar.classList.remove("move-sidebar");
+//     this.previousElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
+
+// closeIcon.addEventListener("click", function(){
+//     sidebar.classList.add("move-sidebar");
+//     this.nextElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
+
+
+
+// let addBtn = document.querySelector(".text button");
+// let inputText = document.querySelector(".text input");
+// let ul = document.querySelector(".text-area");
+// let span = document.querySelector(".text error-message")
+
+
+// addBtn.addEventListener("click", function () {
+//     if (inputText.value.trim() == "") {
+//         // alert("Can't be empty");----------------
+//         span.classList.remove("d-none")
+//         return;
+//     }
+
+//     span.classList.add("d-none")
+
+//     ul.innerHTML = "";
+
+//     for (let i = 1; i <= inputText.value; i++) {
+//         let li = document.createElement("li");
+//         li.className = "list-group-item";
+//         li.innerText = i;
+//         ul.append(li);
+//     }
+
+
+//     inputText.value = "";
+
+//     li.onclick = function () {
+//         this.remove();
+//     }
+// })
+
+
+
+//     =======================================================================================
+//     =======================================================================================  ------ HOMEWORK
+//     =======================================================================================
+
+
+// ------------------------------------------------------------------ RIGHT SIDEBAR
+
+
+// let sidebar = document.querySelector(".sidebar");
+
+// let openIcon = document.querySelector(".sidebar .open-icon");
+
+// let closeIcon = document.querySelector(".sidebar .close-icon");
+
+
+// openIcon.addEventListener("click", function () {
+//     sidebar.classList.remove("move-sidebar");
+//     this.previousElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
+
+// closeIcon.addEventListener("click", function () {
+//     sidebar.classList.add("move-sidebar");
+//     this.nextElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
+
+
+
+
+
+// ----------------------------------------------------------------------- UP SIDEBAR
+
+
+// let sidebar = document.querySelector(".sidebar");
+
+// let openIcon = document.querySelector(".sidebar .open-icon");
+
+// let closeIcon = document.querySelector(".sidebar .close-icon");
+
+
+// openIcon.addEventListener("click", function () {
+//     sidebar.classList.remove("move-sidebar");
+//     this.previousElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
+
+// closeIcon.addEventListener("click", function () {
+//     sidebar.classList.add("move-sidebar");
+//     this.nextElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
+
+
+
+
+
+// ------------------------------------------------------------------------ DOWN SIDEBAR
+
+
+
+// let sidebar = document.querySelector(".sidebar");
+
+// let openIcon = document.querySelector(".sidebar .open-icon");
+
+// let closeIcon = document.querySelector(".sidebar .close-icon");
+
+
+// openIcon.addEventListener("click", function () {
+//     sidebar.classList.remove("move-sidebar");
+//     this.previousElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
+
+// closeIcon.addEventListener("click", function () {
+//     sidebar.classList.add("move-sidebar");
+//     this.nextElementSibling.classList.remove("d-none");
+//     this.classList.add("d-none")
+// })
